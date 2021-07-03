6e2f74fd2e2d6cc4925a7ae291c59b8aaaa6b8d4
Joe Renteria <joe@electronflight.com>
2021-07-03 16:21:20 -0700

    Cat assignment complete

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Write your solution here!
+const cats= [`Milo`,`Otis`,`Garfield`];
+
+function destructivelyAppendCat(){cats.push(`Ralph`)};
+
+function destructivelyPrependCat(){cats.unshift(`Bob`)};
+
+function destructivelyRemoveLastCat(){cats.pop()};
+
+function destructivelyRemoveFirstCat(){cats.shift(0)};
+
+function appendCat(name){  
+    const catsapp = [...cats,`${name}`];
+    return catsapp;};
+
+function prependCat(name){
+    const catspre = [`${name}`,...cats];
+    return catspre;};
+
+function removeLastCat(){
+const catslast = cats.slice(0,2);
+
+return catslast;
+};
+
+function removeFirstCat(){
+    const catsfirst = cats.slice(1);
+    return catsfirst;
+}
+
